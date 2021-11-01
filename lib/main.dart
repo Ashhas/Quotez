6a3d86e1a449f94a0afb0bdd1bc4acdbc700d437
Aschwin B <aschwinbruyning@outlook.com>
2021-11-01 15:27:28 +0100

    Buttons added events to buttons in HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quotez/bloc/home_screen/favorite_button/favorite_bloc.dart';
+import 'package:quotez/bloc/home_screen/home_bloc.dart';
+import 'package:quotez/bloc/home_screen/load_quote_button/quote_button_bloc.dart';
+import 'package:quotez/bloc/simple_bloc_observer.dart';
 import 'package:quotez/ui/home.dart';
 import 'package:quotez/ui/saved_quotes_screen.dart';
 import 'package:quotez/utils/constants/theme_const.dart';
 import 'package:quotez/utils/constants/ui_const.dart';
 
 void main() {
+  //Initialize Bloc Observer
+  Bloc.observer = SimpleBlocObserver();
+
   runApp(const MyApp());
 }
 
@@ -13,14 +21,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: AppTheme.getDefaultTheme(),
-      initialRoute: UiConst.homeScreenRoute,
-      routes: {
-        UiConst.homeScreenRoute: (context) => HomeScreen(),
-        UiConst.savedQuotesScreenRoute: (context) => SavedQuotesScreen(),
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => HomeBloc(),
+        ),
+        BlocProvider(
+          create: (context) => QuoteButtonBloc(
+            homeBloc: BlocProvider.of<HomeBloc>(context),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => FavoriteBloc(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: AppTheme.getDefaultTheme(),
+        initialRoute: UiConst.homeScreenRoute,
+        routes: {
+          UiConst.homeScreenRoute: (context) => HomeScreen(),
+          UiConst.savedQuotesScreenRoute: (context) => SavedQuotesScreen(),
+        },
+      ),
     );
   }
 }
--- lib/ui/widgets/action_button_row.dart
+++ lib/ui/widgets/action_button_row.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quotez/bloc/home_screen/favorite_button/favorite_bloc.dart';
+import 'package:quotez/bloc/home_screen/home_bloc.dart';
+import 'package:quotez/bloc/home_screen/load_quote_button/quote_button_bloc.dart';
+import 'package:quotez/bloc/home_screen/share_button/share_bloc.dart';
 
 class ActionButtonRow extends StatefulWidget {
   const ActionButtonRow({Key? key}) : super(key: key);
@@ -9,12 +14,20 @@ class ActionButtonRow extends StatefulWidget {
 
 class _ActionButtonRowState extends State<ActionButtonRow> {
   @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<QuoteButtonBloc>(context).add(InitiateQuoteButton());
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            BlocProvider.of<FavoriteBloc>(context).add(AddQuoteToFavorites());
+          },
           icon: Icon(
             Icons.favorite_border,
             size: 25,
@@ -22,7 +35,9 @@ class _ActionButtonRowState extends State<ActionButtonRow> {
           ),
         ),
         ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            BlocProvider.of<HomeBloc>(context).add(GetRandomQuote());
+          },
           child: Center(
             child: Icon(
               Icons.sync,
@@ -38,7 +53,9 @@ class _ActionButtonRowState extends State<ActionButtonRow> {
           ),
         ),
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            BlocProvider.of<ShareBloc>(context).add(ShareQuote());
+          },
           icon: Icon(
             Icons.share,
             size: 25,
