dc76d0f599cbc459b8375e8099f6e951e0fdd80e
Aschwin B <aschwinbruyning@outlook.com>
2021-11-01 11:07:33 +0100

    Added theme, ui & color constants to the UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:quotez/ui/home.dart';
 import 'package:quotez/ui/saved_quotes_screen.dart';
+import 'package:quotez/utils/constants/theme_const.dart';
+import 'package:quotez/utils/constants/ui_const.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,13 +15,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: "/home_screen",
+      theme: AppTheme.getDefaultTheme(),
+      initialRoute: UiConst.homeScreenRoute,
       routes: {
-        '/home_screen': (context) => HomeScreen(),
-        '/saved_quotes_screen': (context) => SavedQuotesScreen(),
+        UiConst.homeScreenRoute: (context) => HomeScreen(),
+        UiConst.savedQuotesScreenRoute: (context) => SavedQuotesScreen(),
       },
     );
   }
