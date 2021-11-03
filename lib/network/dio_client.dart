603169f8a7254a1a333294c208146c9530130d7a
Aschwin B <aschwinbruyning@outlook.com>
2021-11-03 02:34:15 +0100

    Implemented QuoteResponse in dio client

--- lib/network/dio_client.dart
+++ lib/network/dio_client.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:quotez/data/model/quote.dart';
+import 'package:quotez/data/model/quote_response.dart';
 
 class DioClient {
   final Dio _dio = Dio(
@@ -11,26 +12,41 @@ class DioClient {
     ),
   );
 
-  Future<Quote?> getRandomQuote() async {
-    Quote? newQuote;
-
+  Future<QuoteResponse> getRandomQuote() async {
     try {
       Response randomQuote = await _dio.get('/random.json');
+      var newQuote = Quote.fromJson(randomQuote.data);
 
-      newQuote = Quote.fromJson(randomQuote.data);
+      return SuccesResponse(newQuote);
     } on DioError catch (e) {
-      if (e.response != null) {
-        log('Dio error!');
-        log('STATUS: ${e.response?.statusCode}');
-        log('DATA: ${e.response?.data}');
-        log('HEADERS: ${e.response?.headers}');
-      } else {
-        // Error due to setting up or sending the request
-        log('Error sending request!');
-        log(e.message);
+      switch (e.type) {
+        case DioErrorType.connectTimeout:
+          log('Dio Error Type: ${e.type.toString()}');
+          return ErrorResponse(e.message);
+
+        case DioErrorType.sendTimeout:
+          log('Dio Error Type: ${e.type.toString()}');
+          return ErrorResponse(e.message);
+
+        case DioErrorType.receiveTimeout:
+          log('Dio Error Type: ${e.type.toString()}');
+          return ErrorResponse(e.message);
+
+        case DioErrorType.response:
+          log('Dio Error Type: ${e.type.toString()}');
+          log('STATUS: ${e.response?.statusCode}');
+          log('DATA: ${e.response?.data}');
+          log('HEADERS: ${e.response?.headers}');
+          return ErrorResponse(e.message);
+
+        case DioErrorType.cancel:
+          log('Dio Error Type: ${e.type.toString()}');
+          return ErrorResponse(e.message);
+
+        case DioErrorType.other:
+          log('Dio Error Type: ${e.type.toString()}');
+          return ErrorResponse(e.message);
       }
     }
-
-    return newQuote;
   }
 }
