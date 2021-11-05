a953ef7cfcbbc0868a862356970482321fcee522
Aschwin B <aschwinbruyning@outlook.com>
2021-11-05 09:53:43 +0100

    Created Splashscreen route

--- lib/utils/constants/ui_const.dart
+++ lib/utils/constants/ui_const.dart
@@ -3,6 +3,7 @@ abstract class UiConst {
   static const appName = 'Quotez';
 
   //Navigation Routes
+  static const splashScreenRoute = "/splash_screen";
   static const homeScreenRoute = "/home_screen";
   static const savedQuotesScreenRoute = "/saved_quotes_screen";
 }
