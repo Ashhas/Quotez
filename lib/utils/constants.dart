9f14b99eb6c104c33052201d3fd6dfcc2b735c87
Aschwin Bruyning <aschwinbruyning@outlook.com>
2022-06-11 18:13:24 +0200

    Change app name

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,6 +4,6 @@ abstract class Constants {
   static const playStoreUrl =
       "https://play.google.com/store/apps/details?id=com.ashhas.quotez";
   static const developerEmail = "bruyningaschwin@gmail.com";
-  static const emailSubjectTemplate = "Code Quotes: ";
+  static const emailSubjectTemplate = "Coding Quotes: ";
   static const appIcon = "assets/app_icon.png";
 }
