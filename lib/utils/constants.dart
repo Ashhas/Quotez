4c908519dcec03fd6b6b3eb93aee6b2509f34387
Aschwin Bruyning <aschwinbruyning@outlook.com>
2022-06-11 18:11:11 +0200

    Add url to open the playstore

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,8 @@
 abstract class Constants {
   static const quoteBoxKey = "quotesBox";
   static const githubRepositoryUrl = "https://github.com/Ashhas/Quotez";
+  static const playStoreUrl =
+      "https://play.google.com/store/apps/details?id=com.ashhas.quotez";
   static const developerEmail = "bruyningaschwin@gmail.com";
   static const emailSubjectTemplate = "Code Quotes: ";
   static const appIcon = "assets/app_icon.png";
